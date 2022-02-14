45e3a811a9e45cd524b05942f2a48fe9b8983e51
Thomas Bivins <bivinsthomas@gmail.com>
2022-02-14 14:43:20 -0500

    pt_entry structure, and other small edits
    Added a pt_entry struct for page table entries (will need attributes within when implementing the algos). Also made a small edit to the file reading process (previously was reading the running mode only if the algo type was VMS)

--- memsim.cpp
+++ memsim.cpp
@@ -7,6 +7,15 @@
 
 #define PAGE_SIZE 4096 //Page sized is assumed 4KB, 4096 Bytes
 
+struct pt_entry
+{
+    int PID;
+    int VPN;
+    int offset;
+    int present;
+    int dirty;
+};
+
 int events_cnt = 0;
 int hits_cnt = 0;
 int fault_cnt = 0;
@@ -17,6 +26,19 @@ int main(int argc, char *argv[]) {
     char* in_file = argv[1];
     int num_frames = atoi(argv[2]);
     std::string alg = argv[3];
+    std::string mode;
+    int p;
+    if (alg == "vms"){
+        p = atoi(argv[4]);
+        mode = argv[5];
+    }
+    else if (alg == "lru" || alg == "fifo"){
+        mode = argv[4];
+    }
+    else{
+        std::cout << "Error. Invalid algorithm.\n";
+        return -1;
+    }
 
 
     if (num_frames <= 0) {
@@ -24,34 +46,40 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
+    /* --------------------------------------------
+    //Also add mode check somewhere here.
+        //Would also be a good idea to add a check if LRU, FIFO have the P parameter when they shouldn't 
+        / VMS doesn't have when it should.
+    ----------------------------------------------- */
+    
     if(alg == "vms"){
-        //Taking an extra parameter for vms
-        int p = atoi(argv[4]);
+        
         
         if((p < 1) || (p > 100)){
             std::cout << "Error. Percentage must be a number between 1 and 100.\n";
             return -1;
         }
-
-        std::string mode = argv[5];
+        
         //Making sure inputs are being retrieved
         std::cout << "infile: " << in_file << "\n"
             << "num_frames: " << num_frames << "\n"
             << "alg: " << alg << "\n"
             << "p: " << p << "\n"
-            << "mode: " << mode << "\n";
+            << "mode: " << mode << "\n\n";
     }
     else{
         
-        std::string mode = argv[4];
         //Making sure inputs are being retrieved
         std::cout << "infile: " << in_file << "\n"
             << "num_frames: " << num_frames << "\n"
             << "alg: " << alg << "\n"
-            << "mode: " << mode << "\n";
+            << "mode: " << mode << "\n\n";
     }
-
     //std::ifstream trace;
+    
+    
+    
+    
     FILE *trace = fopen(in_file, "r");
     if (trace == NULL){
         std::cout << "Failed to open requested file.\n";
@@ -63,12 +91,19 @@ int main(int argc, char *argv[]) {
 
     while(fscanf(trace, "%x %c", &addr, &rw) != EOF){
         ++events_cnt;
+        
+        pt_entry *newEntry = new pt_entry;
         if(strcmp(&rw, "R")){
-            ++reads_cnt;
+            ++reads_cnt; //this will be incremented within the algo during faults i think? but we can keep it here to test
+            newEntry->dirty = 0;
         }else if(strcmp(&rw, "W")){
-            ++writes_cnt;
+            ++writes_cnt; //<-- this one too
+            newEntry->dirty = 1;
         }
-        std::cout << "events: " << events_cnt
-            << "\treads: " << reads_cnt << "\twrites: " << writes_cnt << "\n";
     }
+
+    std::cout << "total memory frames: " << num_frames << "\n"
+        << "events in trace: " << events_cnt << "\n"
+        << "total disk reads: " << reads_cnt << "\n"
+        << "writes: " << writes_cnt << "\n";
 }
